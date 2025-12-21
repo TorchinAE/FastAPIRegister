3f7e327ce3d74dae3e3aef1f73582316613fb14b
Torchin Anton <torchinae@yandex.ru>
2025-12-21 16:17:00 +0300

    add get_dir_by_name, add check new dir

--- scr/dbase/crud_directors.py
+++ scr/dbase/crud_directors.py
@@ -14,8 +14,16 @@ async def get_dirs(session: AsyncSession) -> list[Directors]:
 async def get_dir_to_id(session: AsyncSession, dir_id: int) -> Directors | None:
     return await session.get(Directors, dir_id)
 
+async def get_dir_by_name(session:AsyncSession, name:str) -> Directors |None:
+    stat = select(Directors).where(Directors.name==name)
+    result: Result = await session.execute(stat)
+    director = result.scalar_one_or_none()
+    return director
 
 async def create_dir(session: AsyncSession, dir_in: DirectorSchema) -> Directors:
+    check_dir = await get_dir_by_name(session, dir_in.name)
+    if check_dir:
+        return check_dir
     new_dir = Directors(**dir_in.model_dump())
     session.add(new_dir)
     await session.commit()
