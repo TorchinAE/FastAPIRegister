84ee9b0d0ae780b6b303d94589726efa07931c14
Torchin Anton <torchinae@yandex.ru>
2025-12-23 22:44:40 +0300

    add update manager

---
+++ scr/dbase/crud_manager.py
@@ -0,0 +1,44 @@
+from sqlalchemy import select, Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from scr.dbase.models import Manager
+from scr.dbase.schemas.schemas import ManagerUpdateSchema
+
+
+async def get_managers(session: AsyncSession) -> list[Manager]:
+    stmt = select(Manager).order_by(Manager.name)
+    result: Result = await session.execute(stmt)
+    managers = result.scalars().all()
+    return list(managers)
+
+
+async def get_manager_by_id(session: AsyncSession, id: int) -> Manager | None:
+    return await session.get(Manager, id)
+
+
+async def get_manager_by_name(session: AsyncSession, name: str) -> Manager | None:
+    stmt = select(Manager).where(Manager.name == name)
+    result: Result = await session.execute(stmt)
+    manager = result.scalar_one_or_none()
+    return manager
+
+
+async def update_manager(
+    session: AsyncSession, manager: ManagerUpdateSchema
+) -> Manager | None:
+    upd_manager = await get_manager_by_id(session, manager.id)
+    if not upd_manager:
+        return None
+    for field, value in manager.model_dump(exclude_unset=True).items():
+        if field != "id" and hasattr(upd_manager, field):
+            setattr(upd_manager, field, value)
+    await session.flush()
+    return upd_manager
+
+
+async def delete_manager(session: AsyncSession, manager_id: int) -> Manager | None:
+    manager = await session.get(Manager, manager_id)
+    await session.delete(manager)
+    await session.flush()
+    return manager
